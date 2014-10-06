9cf3cc8e6fee11a62e75449b2813484edac51fa5
Ivshin Aleksei <ivshinalekseim@mail.ru>
2014-10-06 11:20:45 +0400

    Add main service points

--- src/main/java/webservice/AutoshowService.java
+++ src/main/java/webservice/AutoshowService.java
@@ -1,12 +1,18 @@
 package webservice;
 
+import java.sql.SQLException;
+
 import service.car.CarServiceImpl;
 import service.customer.CustomerServiceImpl;
-import service.customer.CustomerServiceImplTest;
 import service.merchant.MerchantServiceImpl;
 import service.sales.SalesServiceImpl;
 import service.store.StoreServiceImpl;
+import domain.car.CarDomain;
+import domain.customer.CustomerDomain;
 import domain.customer.CustomerDomainImpl;
+import domain.merchant.MerchantDomain;
+import domain.sales.SalesDomain;
+import domain.store.StoreDomain;
 
 public class AutoshowService {
 	
@@ -20,5 +26,33 @@ public class AutoshowService {
 		return customerService.findByPassport(series, number);
 	}
 	
+	public CarDomain getCarById(long carId){
+		return carService.get(carId);
+	}
 	
+	public CarDomain addCar(String mark, String model, String modification) throws SQLException{
+		return carService.addCar(mark, model, modification);
+	}
+
+	
+	public void removeCar(String mark, String model, String modification){
+		carService.removeCar(mark, model, modification);;
+	}
+	
+	public CarDomain findOneCar(String mark, String model, String modification){
+		return carService.findOne(mark, model, modification);
+	}
+	
+	public SalesDomain newSaleAndUpdateStore(CustomerDomain customer,
+			MerchantDomain merchant, CarDomain car) throws Exception{
+		return salesService.newSaleAndUpdateStore(customer, merchant, car);
+	}
+	
+	public StoreDomain getStore(CarDomain car){
+		return storeService.get(car);
+	}
+	
+	public MerchantDomain getMerchantById(int merchantId){
+		return merchantService.get(merchantId);
+	}
 }
